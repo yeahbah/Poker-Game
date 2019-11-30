e349e2504a8573f8fdb8e9a41dd76c407969c31f
Arnold Diaz <arnold.diaz@gmail.com>
2019-11-30 12:48:12 -0700

    Deck of cards implementation and test

---
+++ Poker/Deck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Poker
+{
+    public class Deck
+    {
+        public Deck(int numDecks = 1)
+        {
+            InitializeDeck(numDecks);
+        }
+
+        private Card[] _cards;
+        private void InitializeDeck(int numDecks)
+        {
+            // 13 card values 4 suits
+            // 13 x 4 array
+            const int cardRow = 13;
+            const int suitColumn = 4;
+
+            var cardList = new List<Card>();
+            for (var deck=0; deck < numDecks; deck++)
+            {
+                for (var col = 1; col <= suitColumn; col++)
+                {
+                    for (var row = 1; row <= cardRow; row++)
+                    {
+                        var card = new Card((CardValue)row, (Suit)col);
+                        cardList.Add(card);
+                    }
+                }
+            }
+
+            _cards = cardList.ToArray();
+        }
+
+        public Card[] Cards => _cards;
+    }
+}
---
+++ PokerTest/DeckTest.cs
@@ -0,0 +1,64 @@
+﻿using Poker;
+using Shouldly;
+using System.Linq;
+using Xunit;
+
+namespace PokerTest
+{
+    public class DeckTest
+    {
+        [Fact]
+        public void Deck_Contains_52_Cards_Test()
+        {
+            var deck = new Deck();
+            deck.Cards.Length.ShouldBe(52);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MultiDeck_CardCount_Test(int numDecks)
+        {
+            var deck = new Deck(numDecks);
+            deck.Cards.Length.ShouldBe(numDecks * 52);
+        }
+
+        [Fact]
+        public void Deck_Has_Unique_Cards()
+        {
+            var deck = new Deck();
+            var distinctCount = deck.Cards.Distinct().Count();
+            distinctCount.ShouldBe(52);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void Multi_Deck_Test(int numDecks)
+        {
+            var deck = new Deck(numDecks);
+
+            var suitColumn = 4;
+            var cardRow = 13;
+            for (var col = 1; col <= suitColumn; col++)
+            {
+                for (var row = 1; row <= cardRow; row++)
+                {
+                    var card = new Card((CardValue)row, (Suit)col);
+
+                    deck.Cards
+                        .Where(c => c == card)
+                        .Count()
+                        .ShouldBe(numDecks);
+                }
+            }
+
+        }
+    }
+}
