7dd051c744ca902ddfb250ff40bb334eb5ab5fa1
yeahbah <yeahbah@outlook.com>
2020-01-10 09:14:36 -0700

    Video poker web interface initial commit

---
+++ VideoPokerWeb/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Poker;
+using VideoPokerWeb.Models;
+
+namespace VideoPokerWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var deck = new Deck();
+            var hand = deck.TakeCards(5);
+            var videoPoker = new VideoPokerModel
+            {
+                Hand = hand.ToArray(),
+                HoldCardIndex = new int[] {}
+            };
+
+            return View(videoPoker);
+        }
+    }
+}
---
+++ VideoPokerWeb/Models/VideoPokerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Poker;
+
+namespace VideoPokerWeb.Models
+{
+    public class VideoPokerModel
+    {
+        public Card[] Hand { get; set; }
+        public int[] HoldCardIndex { get; set; }
+    }
+}
